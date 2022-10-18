5a790be0548a202614bf8b7f7863a37fdeb857bc
Schaene Trible <schaene@gmx.com>
2022-10-18 18:17:52 -0500

    Fixed backbone
    Previous commit included old code that had errors.

--- highScore.py
+++ highScore.py
@@ -1,24 +1,19 @@
 import os, json
 
+# sets name to user input
 name = input("Enter your name: ") 
+# temporarily hardcoded
 score = 5
 scores_json = []
-dictionary = {
-    "name": name,
-    "score": score
-}
 
-# tempJson = json.dumps(dictionary, indent = 2)
-# print(tempJson)
+# reads the existing file and stores into scores_json
 with open ("highScore.json", "r") as file:
     scores_json = json.load(file) 
     
-
+# appends user input to current json data, writes back to json file
 with open ("highScore.json", "w") as file:
     scores_json.append({
         "name": name,
         "score": score
     })
     json.dump(scores_json, file, indent=2, separators=(',',': '))
-
-print(scores_json)
